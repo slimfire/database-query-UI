1cf1ba43229866299cb999a65cf6a97c336d206e
Yohannes <personalprojectyohannes@gmail.com>
2016-04-14 04:14:02 -0500

    [FEATURE] support AND and OR on where clause

--- public/app.js
+++ public/app.js
@@ -106,7 +106,12 @@ function getTimeRange(){
 var ResultView;
 function appViewModel(){
 	var self = this;
-	self.test = [1, 2, 4, 5, 6, 7, 8, 89, 9]
+	self.queryValues = ko.observableArray([
+			{attribute : ko.observable('')},
+			{attribute : ko.observable('')},
+			{attribute : ko.observable('')},
+			{attribute : ko.observable('')}
+		]);
 	self.attributes = ko.observableArray([]);
 	self.selectedItems = ko.observableArray([]);
 	self.data = ko.observableArray([]);
@@ -118,31 +123,72 @@ function appViewModel(){
 	self.sumOfBytesMax = ko.observable();
 	self.sumOfPacketsMin = ko.observable();
 	self.sumOfPacketsMax = ko.observable();
+	//on query building section
+	self.firstOp = ko.observable('');
+	self.secondOp = ko.observable('');
+	self.thirdOp = ko.observable('');
+	var currentIndex = 0;
+	self.clearQueryBuild = function(){
+		self.queryValues([
+			{attribute : ko.observable('')},
+			{attribute : ko.observable('')},
+			{attribute : ko.observable('')},
+			{attribute : ko.observable('')}
+		]);
+	}
+	self.addToQuery = function(attribute){
+		for (var i = 0; i <= self.queryValues().length - 1; i++) {
+			console.log(self.queryValues()[i].attribute())
+			if(self.queryValues()[i].attribute() == '')
+			{
+				currentIndex = i;
+				break;
+			}
+		};
+		console.log(currentIndex)
+		if(attribute == 'timestamp')
+		{
+			self.queryValues()[currentIndex].attribute(attribute);
+			self.queryValues()[currentIndex]['timestamp'] = {};
+			self.queryValues()[currentIndex]['timestamp']['$gte'] = new Date(self.startDate()).getTime();
+			self.queryValues()[currentIndex]['timestamp']['$lte'] = new Date(self.endDate()).getTime();
+		}
+		else if(attribute == 'dirction_ingress')
+		{
+			self.queryValues()[currentIndex].attribute(attribute);
+			self.queryValues()[currentIndex]['dirction_ingress'] = {};
+			self.queryValues()[currentIndex]['dirction_ingress']['$gte'] = self.directionMin();
+			self.queryValues()[currentIndex]['dirction_ingress']['$lte'] = self.directionMax();
+		}
+		else if(attribute == 'sum_bytes')
+		{
+			self.queryValues()[currentIndex].attribute(attribute);
+			self.queryValues()[currentIndex]['sum_bytes'] = {};
+			self.queryValues()[currentIndex]['sum_bytes']['$gte'] = self.sumOfBytesMin();
+    		self.queryValues()[currentIndex]['sum_bytes']['$lte'] = self.sumOfBytesMax();
+		}
+		else if(attribute == 'sum_packets')
+		{
+    		self.queryValues()[currentIndex].attribute(attribute);
+    		self.queryValues()[currentIndex]['sum_packets'] = {};
+			self.queryValues()[currentIndex]['sum_packets']['$gte'] = self.sumOfPacketsMin();
+			self.queryValues()[currentIndex]['sum_packets']['$lte'] = self.sumOfPacketsMax();
+		}
+		
+		console.log(attribute, self.queryValues())
+	}
 	self.query = function(){
 		console.log(self.selectedItems());
 		console.log(self.startDate(), " <<<--->>> ", self.endDate())
 		var params = {
 	    	select : self.selectedItems(),
-	    	where : {
-	    		timestamp : {
-	    			$gte : new Date(self.startDate()).getTime() || '',
-	    			$lte : new Date(self.endDate()).getTime() || ''
-	    		},
-	    		dirction_ingress : {
-	    			$gte : self.directionMin(),
-	    			$lte : self.directionMax()
-	    		},
-	    		sum_bytes : {
-	    			$gte : self.sumOfBytesMin(),
-	    			$lte : self.sumOfBytesMax()
-	    		},
-	    		sum_packets : {
-	    			$gte : self.sumOfPacketsMin(),
-	    			$lte : self.sumOfPacketsMax()
-	    		}
-	    	}
+	    	where : {}
 	    }
-		
+
+	    params.where[mapOpCode(self.secondOp())] = [{}, {}];
+		params.where[mapOpCode(self.secondOp())][0][mapOpCode(self.firstOp())] = [self.queryValues()[0], self.queryValues()[1]];
+		params.where[mapOpCode(self.secondOp())][1][mapOpCode(self.thirdOp())] = [self.queryValues()[2], self.queryValues()[3]];
+		console.log(params.where)
 		fetch(params.select, params.where, function(data){
 			self.data(data);
 			ResultView = Backbone.View.extend({
@@ -181,5 +227,13 @@ function appViewModel(){
 		};
 		self.attributes(attributes);
 	});
+}
+
+
+var mapOpCode = function(opCode){
+	if(opCode == 'AND')
+		return '$and';
+	if(opCode == 'OR')
+		return '$or';
 }
 ko.applyBindings(new appViewModel());
