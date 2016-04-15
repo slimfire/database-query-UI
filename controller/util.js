48b276a704080a5be843433085f3920af4895662
Yohannes <personalprojectyohannes@gmail.com>
2016-04-15 16:28:44 -0500

    [FIX] update removeEmptyFields to remove empty, undefined and nul fileds/objects

--- controller/util.js
+++ controller/util.js
@@ -2,54 +2,13 @@ const model = require('../models/model');
 
 var Util = function(){}
 Util.prototype.removeEmptyFields = function(where){
-	if(!where.dirction_ingress.$gte)
-	{
-		delete where.dirction_ingress.$gte;
-	}
-	if(!where.dirction_ingress.$lte)
-	{
-		delete where.dirction_ingress.$lte;
-	}
-	if(!where.timestamp.$gte)
-	{
-		delete where.timestamp.$gte;
-	}
-	if(!where.timestamp.$lte)
-	{
-		delete where.timestamp.$lte;
-	}
-	if(!where.sum_bytes.$gte)
-	{
-		delete where.sum_bytes.$gte;
-	}
-	if(!where.sum_bytes.$lte)
-	{
-		delete where.sum_bytes.$lte;
-	}
-	if(!where.sum_packets.$gte)
-	{
-		delete where.sum_packets.$gte;
-	}
-	if(!where.sum_packets.$lte)
-	{
-		delete where.sum_packets.$lte;
-	}
-	if(Object.keys(where.timestamp).length == 0)
-	{
-		delete where.timestamp;
-	}
-	if(Object.keys(where.dirction_ingress).length == 0)
-	{
-		delete where.dirction_ingress;
-	}
-	if(Object.keys(where.sum_bytes).length == 0)
-	{
-		delete where.sum_bytes;
-	}
-	if(Object.keys(where.sum_packets).length == 0)
-	{
-		delete where.sum_packets;
-	}
+    for (var i in where) {
+        if ((i == 'undefined') || (where[i] === '') || (where[i] == null) || (where[i] == {})) {
+            delete where[i];
+        } else if (typeof where[i] === 'object') {
+            Util.prototype.removeEmptyFields(where[i]);
+        }
+    }
 	return where;
 }
 
